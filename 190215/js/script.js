8216590e49d0831c381ac2824cd38fd15cceb505
Ocean <Ocean_Fang@goldengame.com.tw>
2020-03-17 10:43:44 +0800

    final

--- 190215/js/script.js
+++ 190215/js/script.js
@@ -1,13 +1,17 @@
 $(function(){
-    $("nav li.active").click(function(){
+    $("nav li:lt(2)").click(function(){
+       alert("即將公開");
+       return(false);
+    });
+    $("nav li.active, .btn.active").click(function(){
        return(false);
     });
     
     $(".r18").click(function(){
-       alert("以下是18禁的內容");
+       alert("以下是18禁的內容，確定已滿規定之年齡");
     });
     
-    $(".btn").hover(function(){
+    $(".btn:not(.active)").hover(function(){
        $(this).children("a").css("color","#e34858");
     },function(){
        $(this).children("a").css("color","#ffffff");
